ff5ed9be515f95567b76fa846fc2027ed024fb90
Anandhu B <anandhu3390@gmail.com>
2025-12-23 19:07:55 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum of the numbers: ",num1+num2)
+print("Difference of the numbers: ",num1-num2)
+print("Product of the numbers: ",num1*num2)
+print("Division of the numbers: ",num1/num2)
